69b018e87de95a16711f685846172ab243d3df23
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-29 15:52:47 +0200

    Create Order model

--- APBD07/Models/Order.cs
+++ APBD07/Models/Order.cs
@@ -2,5 +2,9 @@ namespace APBD07.Models;
 
 public class Order
 {
-    
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? FulfilledAt { get; set; }
 }
