fbf0734f1522a1f15362e5c02017ce717fd4e39f
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-29 15:38:11 +0200

    add class for Product_Warehouse endpoints

--- APBD07/Endpoints/ProductWarehouseEndpoints.cs
+++ APBD07/Endpoints/ProductWarehouseEndpoints.cs
@@ -1,9 +1,27 @@
+using APBD07.DTOs;
+using FluentValidation;
+
 namespace APBD07.Endpoints;
 
 public static class ProductWarehouseEndpoints
 {
     public static void RegisterProductWarehouseEndpoints(this WebApplication app)
     {
-        
+        var productWarehouse = app.MapGroup("Product_Warehouse");
+        productWarehouse.MapPost("", AddProductWarehouse);
+    }
+
+    private static async Task<IResult> AddProductWarehouse(
+        CreateProductWarehouseDto request,
+        IConfiguration configuration,
+        IValidator<CreateProductWarehouseDto> validator)
+    {
+        var validate = await validator.ValidateAsync(request);
+        if (!validate.IsValid)
+        {
+            return Results.ValidationProblem(validate.ToDictionary());
+        }
+
+        return Results.Created();
     }
 }
