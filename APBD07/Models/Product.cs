4cbb8bf601e9027a8194728c6505d83c205e7a3f
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-29 15:51:31 +0200

    Create Product model

--- APBD07/Models/Product.cs
+++ APBD07/Models/Product.cs
@@ -2,5 +2,8 @@ namespace APBD07.Models;
 
 public class Product
 {
-    
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public double Price { get; set; }
 }
