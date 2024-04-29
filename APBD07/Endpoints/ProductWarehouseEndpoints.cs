26db0f8b89717a3117b74a89c96eba5253deff47
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-29 14:45:49 +0200

    Product_Warehouse endpoints class

---
+++ APBD07/Endpoints/ProductWarehouseEndpoints.cs
@@ -0,0 +1,9 @@
+namespace APBD07.Endpoints;
+
+public static class ProductWarehouseEndpoints
+{
+    public static void RegisterProductWarehouseEndpoints(this WebApplication app)
+    {
+        
+    }
+}
