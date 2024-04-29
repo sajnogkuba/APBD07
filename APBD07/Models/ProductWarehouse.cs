d8e19f4b60608e39a0f171716bd2b8f4298c18a9
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-29 15:48:15 +0200

    Create Product_Warehouse model

--- APBD07/Models/ProductWarehouse.cs
+++ APBD07/Models/ProductWarehouse.cs
@@ -2,5 +2,12 @@ namespace APBD07.Models;
 
 public class ProductWarehouse
 {
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int OrderId { get; set; }
+    public int WarehouseId { get; set; }
+    public int Amount { get; set; }
+    public double Price { get; set; }
+    public DateTime CreatedAt { get; set; }
     
 }
